21c1b2a6b79cc3893a26d8e84d456f1dc5edfac0
BorislavChernev <borocool359@gmail.com>
2022-11-06 21:37:53 +0200

    Completed Task

---
+++ JQuery/Change-the-CSS-of-an-Element-Using-jQuery/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+	$('button').addClass('animated bounce');
+	$('.well').addClass('animated shake');
+	$('#target3').addClass('animated fadeOut');
+	$('button').removeClass('btn-default');
+	$('#target1').css('color', 'red');
+});
