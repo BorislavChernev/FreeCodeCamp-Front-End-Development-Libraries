e64ae00cf0b4326d1a6be245b368e5ed38c09312
BorislavChernev <borocool359@gmail.com>
2022-11-07 19:42:47 +0200

    Completed Task

---
+++ JQuery/Clone-an-Element-Using-jQuery/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+	$('#target1').css('color', 'red');
+	$('#target4').html('<em>#target4</em>');
+	$('#target4').remove();
+	$('#target2').appendTo('#right-well');
+	$('#target5').clone().appendTo('#left-well');
+});
