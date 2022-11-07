f8306b34ff885e67dfe0c92bf523ceac113b2e73
BorislavChernev <borocool359@gmail.com>
2022-11-07 19:43:13 +0200

    Completed Task

---
+++ JQuery/Front-End-Development-Libraries-jQuery-Target-the-Parent-of-an-Element-Using-jQuery/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	$('#target1').css('color', 'red');
+	$('#target4').html('<em>#target4</em>');
+	$('#target4').remove();
+	$('#target2').appendTo('#right-well');
+	$('#target5').clone().appendTo('#left-well');
+	$('#target1').parent().css('background-color', 'red');
+});
