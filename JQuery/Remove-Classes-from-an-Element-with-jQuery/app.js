a94fa8e462a2d687300ad3b6139532c370ff5278
BorislavChernev <borocool359@gmail.com>
2022-11-09 17:21:58 +0200

    Completed Task

---
+++ JQuery/Remove-Classes-from-an-Element-with-jQuery/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+	$('button').addClass('animated');
+	$('.btn').addClass('shake');
+	$('#target1').addClass('btn-primary');
+
+	$('button').removeClass('btn-default');
+});
