c65be1d4b1b3a07802d413ce32961dad79f84aa9
BorislavChernev <borocool359@gmail.com>
2022-11-09 17:22:10 +0200

    Completed Task

---
+++ JQuery/Target-a-Specific-Child-of-an-Element-Using-jQuery/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('#target1').css('color', 'red');
+	$('#target4').html('<em>#target4</em>');
+	$('#target4').remove();
+	$('#target2').appendTo('#right-well');
+	$('#target5').clone().appendTo('#left-well');
+	$('#target1').parent().css('background-color', 'red');
+	$('#right-well').children().css('color', 'orange');
+    $('.target:nth-child(2)').addClass('animated bounce');
+});
