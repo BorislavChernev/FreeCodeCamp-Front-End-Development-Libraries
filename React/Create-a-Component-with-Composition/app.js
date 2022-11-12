c1b380059acd932088a75e6ca2f59d9fd9663df1
BorislavChernev <borocool359@gmail.com>
2022-11-12 23:05:30 +0200

    Completed Task

---
+++ React/Create-a-Component-with-Composition/app.js
@@ -0,0 +1,22 @@
+const ChildComponent = () => {
+	return (
+		<div>
+			<p>I am the child</p>
+		</div>
+	);
+};
+
+class ParentComponent extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div>
+				<h1>I am the parent</h1>
+				<ChildComponent />
+			</div>
+		);
+	}
+}
+ReactDOM.render(<MyComponent />, document.getElementById('root'));
