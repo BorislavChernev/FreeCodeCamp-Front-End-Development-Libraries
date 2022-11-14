c47d950091bc8735aa905bb34dde4ee50d6e5c5b
BorislavChernev <borocool359@gmail.com>
2022-11-14 17:45:06 +0200

    Completed Task

---
+++ React/Review-Using-Props-with-Stateless-Functional-Components/app.js
@@ -0,0 +1,19 @@
+class CampSite extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div>
+				<Camper />
+			</div>
+		);
+	}
+}
+
+const Camper = (props) => <p>{props.name}</p>;
+
+Camper.defaultProps = { name: 'CamperBot' };
+Camper.propTypes = { name: PropTypes.string.isRequired };
+
+ReactDOM.render(<CampSite />, document.getElementById('challenge-node'));
