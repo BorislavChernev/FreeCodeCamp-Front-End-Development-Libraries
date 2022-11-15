bcc5c9a27473270c7a489739df1f3ce3525fdd5d
BorislavChernev <borocool359@gmail.com>
2022-11-15 20:00:39 +0200

    Completed Task

---
+++ React/Use-React-to-Render-Nested-Components/app.js
@@ -0,0 +1,34 @@
+const TypesOfFruit = () => {
+	return (
+		<div>
+			<h2>Fruits:</h2>
+			<ul>
+				<li>Apples</li>
+				<li>Blueberries</li>
+				<li>Strawberries</li>
+				<li>Bananas</li>
+			</ul>
+		</div>
+	);
+};
+
+const Fruits = () => {
+	return <div></div>;
+};
+
+class TypesOfFood extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Types of Food:</h1>
+				<Fruits />
+				<TypesOfFruit />
+			</div>
+		);
+	}
+}
+ReactDOM.render(<TypesOfFood />, document.getElementById('root'));
