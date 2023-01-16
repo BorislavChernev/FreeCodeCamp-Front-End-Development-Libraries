233c115e29ae445265965154cfb293537550d037
BorislavChernev <borocool359@gmail.com>
2023-01-16 19:08:26 +0200

    Completed Task

---
+++ React-Redux/Connect-Redux-to-React/app.js
@@ -0,0 +1,36 @@
+const addMessage = (message) => {
+	return {
+		type: 'ADD',
+		message: message,
+	};
+};
+
+const mapStateToProps = (state) => {
+	return {
+		messages: state,
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		submitNewMessage: (message) => {
+			dispatch(addMessage(message));
+		},
+	};
+};
+
+class Presentational extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return <h3>This is a Presentational Component</h3>;
+	}
+}
+
+const connect = ReactRedux.connect;
+
+const ConnectedComponent = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(Presentational);
