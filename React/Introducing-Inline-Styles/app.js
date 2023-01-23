2a56dcd4d7ddb55556699d2bfcf18fe7127fc2b2
BorislavChernev <borocool359@gmail.com>
2023-01-23 22:46:29 +0200

    Completed Task

---
+++ React/Introducing-Inline-Styles/app.js
@@ -0,0 +1,6 @@
+class Colorful extends React.Component {
+	render() {
+		return <div style={{ color: 'red', fontSize: 72 }}>Big Red</div>;
+	}
+}
+ReactDOM.render(<Colorful />, document.getElementById('challenge-node'));
