05479888a1764032d5a80b77e4e8e84a36581dd8
BorislavChernev <borocool359@gmail.com>
2023-01-30 22:54:48 +0200

    Completed Task

---
+++ Redux/Remove-an-Item-from-an-Array/app.js
@@ -0,0 +1,20 @@
+const immutableReducer = (state = [0, 1, 2, 3, 4, 5], action) => {
+	switch (action.type) {
+		case 'REMOVE_ITEM':
+			return [
+				...state.slice(0, action.index),
+				...state.slice(action.index + 1, state.length),
+			];
+		default:
+			return state;
+	}
+};
+
+const removeItem = (index) => {
+	return {
+		type: 'REMOVE_ITEM',
+		index,
+	};
+};
+
+const store = Redux.createStore(immutableReducer);
